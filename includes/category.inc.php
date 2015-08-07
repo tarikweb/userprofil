043dfa8d1e4c56b2eafab654651bef809e7ec328
amine El houba <amine142@hotmail.fr>
2015-08-07 14:30:31 +0200

    ajout modele de donnée categorie

---
+++ includes/category.inc.php
@@ -0,0 +1,7 @@
+<?php
+// fonction edition d'une catégorie
+function category_edit($id, $db_connexion){
+$statement = $db_connexion->query("SELECT * FROM categorie where id_categorie = $id;");
+$categorie = $statement->fetch();
+return $categorie;
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'includes/db.inc.php';
 include 'includes/user.inc.php';
+include 'includes/category.inc.php';
 $out = "";
 if (!empty($_SESSION["user_session"])) {
 
@@ -29,8 +30,6 @@ else if(empty($_SESSION)){
             </div>
         </form>';
 }
-
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -48,28 +47,17 @@ else if(empty($_SESSION)){
                  </div>
             </div>
             <?php
-            $where = !empty($_GET['id'])?"where id_categorie =".$_GET["id"]:" where 1";
-
-
-$statement = $db_connexion->query("SELECT * FROM categorie ORDER BY niveau ASC;");
-$categories = $statement->fetchAll();
-
-function returnCategoryName($id, $db_connexion){
-$statement = $db_connexion->query("SELECT * FROM categorie where id_categorie = $id;");
-$categorie = $statement->fetch();
-return $categorie["nom"];
-}
-
-echo "<div class='col-md-3'>
-<ul>";
+            $where = isset($_GET['id'])?"where id_categorie =".$_GET["id"]:" where 1";
+            $statement = $db_connexion->query("SELECT * FROM categorie ORDER BY niveau ASC;");
+            $categories = $statement->fetchAll();
+    echo "<div class='col-md-3'>
+          <ul>";
          foreach($categories as $categorie){
-         
-          if ($categorie["niveau"] == 1){ // 1er niveau
-            $plus = '--';
-           echo "<li><a href='?id=".$categorie["id_categorie"]."'>
-                ".$plus.' '.utf8_encode($categorie["nom"])
-               ."</a></li>";
-             // 2 eme niveau
+          if($categorie["niveau"] == 1){ // 1er niveau
+              $plus = '--';
+              echo "<li><a href='?id=".$categorie["id_categorie"]."'>   ".$plus.' '.utf8_encode($categorie["nom"])."</a></li>";
+             
+              // 2 eme niveau
               $query = "SELECT * FROM categorie where id_parent=:id_categorie";//
               $statement = $db_connexion->prepare($query);
               $statement->bindParam(":id_categorie",$categorie["id_categorie"]); // Liaison param :nom , et $_POST["nom"]
@@ -77,10 +65,9 @@ echo "<div class='col-md-3'>
                   $results = $statement->fetchAll(); // Methode fetch de l'objet statement 
                   foreach($results as $result){
                     $plus ='----';
-                     echo "<li><a href='?id=".$result["id_categorie"]."'>
-                ".$plus.' '.utf8_encode($result["nom"])
-               ."</a></li>";
-                  // 3 eme niveau
+                    echo "<li><a href='?id=".$result["id_categorie"]."'>".$plus.' '.utf8_encode($result["nom"])."</a></li>";
+
+              // 3 eme niveau
               $query = "SELECT * FROM categorie where id_parent=:id_categorie";// lower fonction sql qui met les caractére en miniscule
               $statement = $db_connexion->prepare($query);
               $statement->bindParam(":id_categorie",$result["id_categorie"]); // Liaison param :nom , et $_POST["nom"]
@@ -88,43 +75,37 @@ echo "<div class='col-md-3'>
                   $results = $statement->fetchAll(); // Methode fetch de l'objet statement 
                   foreach($results as $result){
                     $plus ='------';
-                     echo "<li><a href='?id=".$result["id_categorie"]."'>
-                ".$plus.' '.utf8_encode($result["nom"])
-               ."</a></li>";
+                     echo "<li><a href='?id=".$result["id_categorie"]."'>".$plus.' '.utf8_encode($result["nom"])."</a></li>";
                    }
-                  }
-          }
+              }
+            }
          }
 
 echo "</ul>
      </div>";
 
 echo "<div class='col-md-9'>";
-echo "<table>";
-echo "<thead>
-      <tr>
-      <th>Nom</th>
-      <th>Réference</th>
-      <th>Prix HT</th>
-       <th>Prix TTC</th>
-       <th>Catégorie</th> ";
-echo "</tr>
-      </thead>";
-echo "<tbody>";
+
+if(isset($_GET["id"])){
+   echo "<p>".utf8_encode(category_edit($_GET["id"], $db_connexion))."</p> "; 
+}
+
+
 $statement = $db_connexion->query("SELECT * FROM produit $where;");
 $produits = $statement->fetchAll();
 
      foreach($produits as $produit){
-         echo "<tr>
-          <td>".$produit["nom"]."</td> 
-          <td>".$produit["reference"]."</td> 
-          <td>".$produit["prix"]."</td> 
-          <td>".$produit["prixht"]."</td> 
-          <td>".utf8_encode(returnCategoryName($produit["id_categorie"], $db_connexion))."</td> 
-         </tr>";
+         echo "<div >
+          <p>".$produit["nom"]."</p> 
+          <p>".$produit["reference"]."</p> 
+          <p>".$produit["prix"]."</p> 
+          <p>".$produit["prixht"]."</p> ";
+         $category = category_edit($produit["id_categorie"], $db_connexion);
+         echo "<p>".utf8_encode($category["nom"])."</p> 
+         </div>";
      }
-echo "</tbody>
-      </table>";
+echo "
+      ";
 echo "</div>"; 
 ?>
         </div>
